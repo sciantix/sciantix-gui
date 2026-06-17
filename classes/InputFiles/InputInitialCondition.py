c441999814472d847e9ef3ca381f4e187a7738f4
Zaynn-lea <gely.leandre@gmail.com>
2026-06-17 14:32:14 +0200

    completed the InputInitialCondition class

--- classes/InputFiles/InputInitialCondition.py
+++ classes/InputFiles/InputInitialCondition.py
@@ -23,3 +23,25 @@ from . import InputFile
 class InputInitialCondition(InputFile):
     def __init__(self):
         super().__init__("input_initial_condition")
+
+        # Variable to represent the layout of data from the input_initial_condition.txt input file
+        # Each element of the array represent the number of factor on each lines of input_initial_condition.txt
+        self.__layout = [
+            1,
+            6,
+            6,
+            6,
+            2,
+            1,
+            1,
+            1,
+            1,
+            5,
+            7,
+            7,
+            1
+        ]
+
+
+        def getLayout(self) -> list[int]:
+            return self.__layout
