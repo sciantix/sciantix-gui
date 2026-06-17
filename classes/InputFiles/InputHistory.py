4a0cd374cd061a1a79b6674cfacc09966f6a3cc9
Zaynn-lea <gely.leandre@gmail.com>
2026-06-17 15:01:24 +0200

    InputHistory class structure

--- classes/InputFiles/InputHistory.py
+++ classes/InputFiles/InputHistory.py
@@ -21,5 +21,32 @@ from . import InputFile
 
 
 class InputHistory(InputFile):
-    def __init__(self):
+    def __init__(self, nbr_lines: int, has_steam_pressure: bool = False):
         super().__init__("input_history")
+
+        self.__nbr_lines          = nbr_lines
+        self.__has_steam_pressure = has_steam_pressure
+
+    
+    def getNbrLines(self) -> int:
+        return self.__nbr_lines
+    
+    def getLineByNbr(self, index: int):
+        if not (0 <= index < self.__nbr_lines):
+            raise IndexError
+
+        pass        # TODO
+    
+    def addLine(self):
+        self.__nbr_lines += 1
+
+        # TODO
+    
+    def deleteLine(self, index: int):
+        if not (0 <= index < self.__nbr_lines):
+            raise IndexError
+
+        self.__nbr_lines -= 1
+
+        # TODO
+
